2b23c209cc38c779950c3b37bd34c6519fa565b3
Aakriti Kumari <aakritikumari@Aakritis-MacBook-Air.local>
2023-12-23 20:31:08 +0530

    Creating folder sturucture to save java practice files.

---
+++ Crux/src/com/crux/EvenOdd.java
@@ -0,0 +1,18 @@
+package com.crux;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if(number%2 == 0) {
+			System.out.println(number+" is an even number!");
+		}
+		else {
+			System.out.println(number+" is an odd number!");
+		}
+	}
+
+}
---
+++ Crux/src/com/crux/Sum_N_Numbers.java
@@ -0,0 +1,19 @@
+package com.crux;
+
+import java.util.Scanner;
+
+public class Sum_N_Numbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int n = number;
+		int sum = 0;
+		while(number>0) {
+			sum += number;
+			number--;
+		}
+		System.out.println("Sum of "+n+" natural numbers: "+sum);
+	}
+
+}
